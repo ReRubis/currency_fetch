64959d9867599e877001d820c5d77760058ac3f5
ReRubis <slesarenkod@gmail.com>
2024-02-11 01:56:31 +0400

    Get routes from app

--- tests/test_testclient.py
+++ tests/test_testclient.py
@@ -1,2 +1,9 @@
 from fastapi.testclient import TestClient
-from webcur.main import app
+from webcur.main import app_factory
+
+
+app = app_factory()
+client = TestClient(app)
+routes = app.routes
+
+print([route.path for route in routes])
